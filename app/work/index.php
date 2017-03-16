be1bb1055ab9d9677ed632a2fb72ed376ce6d976
Kevin Wecker <kwecker@gmail.com>
2017-03-16 15:38:09 -0700

    Really added Our Work

---
+++ app/work/index.php
@@ -0,0 +1,144 @@
+<?php $section = 'Our Work'; ?>
+<?php require_once('../common/header.php'); ?>
+
+		<section class="main-content">
+			<section id="intro" class="container">
+				<h1>Our Work</h1>
+				<p class="loud">
+					<strong>The Wecker Group</strong> is an independent design studio/ad agency established in 1974. We specialize in corporate identity, collateral, print and TV advertising, web design, packaging and event promotion. As one of the area&#8217;s most recognized agencies and winner of numerous local and national awards, The Wecker Group is proud of its consistently high standards and memorable results.
+				</p>
+			</section> <!-- /intro -->
+
+			<hr>
+
+			<section id="home-gallery" class="container">
+				<h2>Logos</h2>
+				<div class="boxes">
+					<div class="row">
+						<div class="box three columns">
+							<a href="" class="box__link">
+								<img src="" alt="" class="box__image">
+								<span class="box__text"></span>
+							</a>
+						</div>
+						<div class="box three columns">
+							<a href="" class="box__link">
+								<img src="" alt="" class="box__image">
+								<span class="box__text"></span>
+							</a>
+						</div>
+						<div class="box three columns">
+							<a href="" class="box__link">
+								<img src="" alt="" class="box__image">
+								<span class="box__text"></span>
+							</a>
+						</div>
+						<div class="box three columns">
+							<a href="" class="box__link">
+								<img src="" alt="" class="box__image">
+								<span class="box__text"></span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<hr>
+
+			<section id="home-gallery" class="container">
+				<h2>Branding</h2>
+				<div class="boxes row">
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<hr>
+
+			<section id="home-gallery" class="container">
+				<h2>Environmental</h2>
+				<div class="boxes row">
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<hr>
+
+			<section id="home-gallery" class="container">
+				<h2>Web Design</h2>
+				<div class="boxes row">
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+					<div class="box three columns">
+						<a href="" class="box__link">
+							<img src="" alt="" class="box__image">
+							<span class="box__text"></span>
+						</a>
+					</div>
+				</div>
+			</section>
+
+		</section>
+
+<?php require_once('../common/footer.php'); ?>
