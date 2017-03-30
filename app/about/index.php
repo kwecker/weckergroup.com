b7f771ab9bcbfde1649f4c9b9366592489e9f56c
Kevin Wecker <kwecker@gmail.com>
2017-03-30 14:44:07 -0700

    Add About Us section.

---
+++ app/about/index.php
@@ -0,0 +1,80 @@
+<?php $section = 'About Us'; ?>
+<?php require_once('../common/header.php'); ?>
+
+		<section class="main-content">
+			<section id="intro" class="container">
+				<h1>About Us</h1>
+				<p class="loud">
+					<strong>The Wecker Group</strong> is an independent design studio/ad agency established in 1974. We specialize in corporate identity, collateral, print and TV advertising, web design, packaging and event promotion. As one of the area&#8217;s most recognized agencies and winner of numerous local and national awards, The Wecker Group is proud of its consistently high standards and memorable results.
+				</p>
+			</section> <!-- /intro -->
+
+			<hr>
+
+			<section id="bios" class="container">
+				<h2>The Team</h2>
+				<div class="row">
+					<div class="six columns team__member group">
+						<img src="" alt="">
+						<h3>Robert Wecker</h3>
+						<div>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, dolorum explicabo dicta aspernatur, commodi odit ex aperiam ea sit rem inventore modi temporibus sapiente, eligendi nobis iure deleniti debitis sed?
+							</p>
+						</div>
+					</div>
+					<div class="six columns team__member group">
+						<img src="" alt="">
+						<h3>Sue Frerkson-Wecker</h3>
+						<div>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio unde alias, eaque quaerat. Enim hic, molestias aliquid maxime, iusto sequi quae explicabo eum, corrupti officiis dolorum labore esse ex adipisci!
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="six columns team__member group">
+						<img src="" alt="">
+						<h3>Ruth Minerva</h3>
+						<div>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quis eos repudiandae beatae, laborum facere vero pariatur aut quam molestiae deleniti voluptas possimus suscipit, nemo natus voluptatem fugiat, molestias dicta?
+							</p>
+						</div>
+					</div>
+					<div class="six columns team__member group">
+						<img src="" alt="">
+						<h3>Matt Gnibus</h3>
+						<div>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quis, itaque expedita deleniti vitae voluptate tempora voluptates, aspernatur quaerat culpa dolor incidunt magni. Nesciunt itaque, dolor doloremque quibusdam sint eos!
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="six columns team__member group">
+						<img src="" alt="">
+						<h3>Kevin Wecker</h3>
+						<div>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat dolorem cumque veniam quos saepe temporibus. Similique, in! Esse laboriosam necessitatibus, eveniet, temporibus eum porro aperiam illo suscipit, cupiditate voluptatem quae.
+							</p>
+						</div>
+					</div>
+					<div class="six columns team__member group">
+						<img src="" alt="">
+						<h3>Lin Sponsler</h3>
+						<div>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. A maiores eaque, commodi enim quasi. Dolorum similique voluptate tempore enim, iure id suscipit aliquam ex quibusdam quos. Nostrum expedita accusamus nesciunt.
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		</section>
+
+<?php require_once('../common/footer.php'); ?>
